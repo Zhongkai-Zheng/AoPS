0dbb99363459e6b38a07df49517be949c9b27704
Alex Zhongkai Zheng <Alex_Zheng@Zhongkai.local>
2016-02-11 00:39:19 -0500

    added a bunch of files

---
+++ client/login.js
@@ -0,0 +1,21 @@
+Template.login.events({
+
+	'click #register': function(event){
+			event.preventDefault();
+			Router.go('register');
+		},
+
+	'submit form': function(event){
+		event.preventDefault();
+		email = $('[name=email]').val();
+		var password = $('[name=password]').val();
+		Meteor.loginWithPassword(email, password, function(error){
+			if(error){
+				console.log(error.reason);
+			} 
+			else{
+				Router.go("home");
+			}
+		});
+	}
+});
